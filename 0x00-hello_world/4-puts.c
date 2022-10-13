69046ed03b1a30726d1555d9f11166bd0a943c12
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-13 14:39:25 -0700

    co

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/**printing in C without using printf**/
+int puts(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/**this is a program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line using printf**/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
