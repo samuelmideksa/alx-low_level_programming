35ff137e5d644ae8a3ee2aaf20e4587646b1acf0
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-15 05:35:18 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /**printing in C without using printf**/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzln");
+	return (0);
 }
