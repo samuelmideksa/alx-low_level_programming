4b1a5a4ff4f73dcb39ba17648753479accdba902
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-15 05:42:06 -0700

    puts function description added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/**printing in C without using printf**/
+/**
+ *printing in C without using printf
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
