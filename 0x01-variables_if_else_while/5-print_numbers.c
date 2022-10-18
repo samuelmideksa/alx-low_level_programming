3dd42d422d5bd7bf8f6bc5852086f9cf373a0a7c
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-18 11:57:05 -0700

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print 0 1 2 3 4 5 6 7 8 9
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int digit;
+
+	digit = 0;
+	while (digit <= 9)
+	{
+		printf("%d\n", digit)
+		digit++;
+	}
+	return (0);
+}
