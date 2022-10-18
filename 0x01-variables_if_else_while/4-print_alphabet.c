d18bf139ecb74ef3336c0e673872961cc4c812be
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-18 11:34:06 -0700

    print with exception

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet but e and q
+ *
+ * Return: o
+ */
+int main(void)
+{
+	char al;
+
+	char al = 'a'
+	while (al <= 'z')
+	{
+		if (al != 'e' && al != 'q')
+		{
+			putchar(al);
+		}
+		al++
+	}
+	putchar('\n');
+	return (0);
+}
