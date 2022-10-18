c89f2950ba1c431827be4c6c31e790af2f2d4764
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-18 09:04:11 -0700

    betty spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,15 +19,15 @@ int main(void)
 
 	n = rand() - RAND_MAX / 2;
 
-	if ( n > 0 ) 
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if ( n == 0 ) 
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else 
+	else
 	{
 		printf("%d is negative\n", n);
 	}
