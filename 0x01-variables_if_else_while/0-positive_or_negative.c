fce2b5afca6477c5cb38a1ab40b0c1c378a02357
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-18 08:49:10 -0700

    positve or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - print random number and specify sign
+ *
+ * Return: zero
+ */
+int main(void)
+
+{
+
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if ( a > 0) {
+		printf("%d is positive\n", n)
+	} else if ( a == 0 ) {
+		printf("%d is zero\n", n)
+	} else {
+		printf("%d is negative\n", n)
+	}
+	return (0);
+
+}
