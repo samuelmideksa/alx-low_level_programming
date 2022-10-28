e21370a4cf2ca9b25226442996aaf6182fe7f156
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-28 07:50:09 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print 00 to 99,separated by a comma
+ * 	  and in ascending order
+ *
+ * Return: 0 always.
+ */
+int main (void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			putchar(', ');
+		}
+	}
+	return (0);
+}
