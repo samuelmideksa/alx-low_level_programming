5fda5ba292b27f72335e94ccb8f3495b52dca696
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-28 08:07:11 -0700

    co

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,11 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
-
-		putchar(',' + ' ');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
