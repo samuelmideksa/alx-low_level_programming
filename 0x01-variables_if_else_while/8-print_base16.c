fddc87396f576a3f9ce566e80f8b59630e295cc1
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-28 07:06:43 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print all hexadecimal numbers in lowercase
+ *
+ * retunr: zero
+ */
+int main(void)
+{
+	char hex;
+	for(hex = 0; hex++)
+	{
+		putchar(hex);
+	}
+	putchar('\n')
+	return (0);
+}
