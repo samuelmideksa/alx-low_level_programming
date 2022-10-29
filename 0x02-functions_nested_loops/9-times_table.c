f891e88480a9ae00c08e0c2fcc7c148cfdad9635
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-29 10:16:50 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * times_table - print time table
+  */
+
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(' ');
+
+		for (mult = 0; num <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
