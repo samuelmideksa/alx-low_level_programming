d135166c5a2fd50370ea6eacb6ff8371b402f39d
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-29 06:41:20 -0700

    ablsoute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * abs - compute absolute value of an integer
+  *
+  * Return: the ablsolute value of the integer
+  */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
