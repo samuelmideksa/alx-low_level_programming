1e6c3ea70dace8b9497e3f5eb813beb3134a7715
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-29 05:23:53 -0700

    ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - print the alphabet ten times
+  */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char alpha;
+
+	while (count++ <= 9)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+	}
+	_putchar('\n');
+}
