5b9ea0e26142ea9e0f9015f162ec11ec7869db3b
samuelmideksa <samuelmideksa4@gmail.com>
2022-10-29 23:59:21 -0700

    fibo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  * main - prints the first 50 fibonacci numbers, followed by a new line
+  *
+  * Return: 0 always.
+  */
+
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
