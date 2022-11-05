6a7115c2bbcec7b3267b0ab794d4089355c92e28
samuelmideksa <samuelmideksa4@gmail.com>
2022-11-05 14:14:09 -0700

    finished

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,7 +17,7 @@ void print_diagonal(int n)
 				_putchar(' ');
 			_putchar('\\');
 
-			if (len == n -1)
+			if (len == n - 1)
 				continue;
 
 			_putchar('\n');
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * print_square - prints a squeriseing charachter # on the terminal.
+  * @size: size of the square.
+  */
+
+void print_square(int size)
+{
+	int hig, wid;
+
+	if (size > 0)
+	{
+		for (hig = 0; hig < size; hig++)
+		{
+			for (wid = 0; wid < hig; wid++)
+				_putchar('#');
+
+			if (hig == size - 1)
+				continue;
+
+			_putchar('\n');
+		}
+
+	}
+
+	_putchar('\n');
+}
