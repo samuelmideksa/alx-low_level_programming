5c2e243b408b420f6887a5f559f91cb06beaf35d
samuelmideksa <samuelmideksa4@gmail.com>
2022-11-05 13:02:36 -0700

    fixed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,14 @@
 /**
   * print_numbers - print numbers 0123456789 followed by a new line
   * @n: numbers
-  * Return: 0 always.
   */
 
-int print_numbers(int n)
+void print_numbers(void)
 {
+	int n;
+
 	for (n = 0; n <= 9; n++)
 		_putchar((n % 10) + '0');
 
 	_putchar('\n');
-
-	return (0);
 }
