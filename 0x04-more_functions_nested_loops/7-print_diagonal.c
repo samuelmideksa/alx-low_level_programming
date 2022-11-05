aea6528ce2a580c686863b8e71a58b00098906d8
samuelmideksa <samuelmideksa4@gmail.com>
2022-11-05 13:41:58 -0700

    completed task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_line - print line in the terminal
+  * @n: umber of dash
+  */
+
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('\');
+	}
+
+	_putchar('\n');
+}
