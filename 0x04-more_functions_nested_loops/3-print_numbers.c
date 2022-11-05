ee82127e701da6118c9bf81aed5d98f0a49ecd5e
samuelmideksa <samuelmideksa4@gmail.com>
2022-11-05 12:53:37 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * print_numbers - print numbers 0123456789 followed by a new line
+  * @n: numbers
+  * Return: 0 always.
+  */
+
+int print_numbers(int n)
+{
+	for (n = 0; n <= 9; n++)
+		_putchar(n);
+}
