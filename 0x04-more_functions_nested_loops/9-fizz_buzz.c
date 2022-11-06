1e5b961ca7b9eeac9a877894c5431374c740fb43
samuelmideksa <samuelmideksa4@gmail.com>
2022-11-05 23:43:29 -0700

    presision error fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,14 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3) == 0)
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+
+		else if ((num % 3) == 0)
 			printf("Fizz");
 
 		else if ((num % 5) == 0)
 			printf("Buzz");
-
-		else if ((num % 3) == 0 && (num % 5) == 0)
-			printf("FizzBuzz");
 
 		else
 			printf("%d", num);
@@ -30,6 +30,8 @@ int main(void)
 
 		printf(" ");
 	}
+
+	printf("\n");
 
 	return (0);
 }
